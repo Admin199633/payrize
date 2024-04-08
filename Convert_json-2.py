e2dbd5913f96e7fa4a8d76c28628a60846fb1db6
Admin199633 <108216254+Admin199633@users.noreply.github.com>
2024-04-09 01:45:06 +0300

    Update Convert_json-2.py

--- Convert_json-2.py
+++ Convert_json-2.py
@@ -1,7 +1,11 @@
 import pandas as pd
-path= "C:\\Users\\LS\\PycharmProjects\\pythonProject5\\"
+ame_xl= input("Please enter name XL: ")
+name_xl="Export_8_04_2024.xlsx"
+path = 'C:\\Users\\LS\\PycharmProjects\\rizeup1\\modified_excel_file.xlsx'
+# Load the Excel workbook
+
 # טען את קובץ האקסל
-df = pd.read_excel('modified_excel_file.xlsx')
+df = pd.read_excel(path)
 # המר את הנתונים לפורמט JSON ושימוש בעברית
 json_data = df.to_json(orient='records', force_ascii=False)
 
